7267900e5449340009767baf1336c4a4e2bf6634
Yoh-Yoh-Honey-Bunny <johannes.stinhoefer@gmx.de>
2026-04-01 15:24:09 +0200

    Bearbeitung Aufgaben 1-10

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR"
+}
+if (num1 < 0 || num2 < 0){
+    return "ERROR"
+}
+const lower = Math.min(num1, num2)
+const higher = Math.max(num1,num2)
 
+let sum = 0
+for (let i= lower; i <= higher; i++) {
+    sum += i;
+}
+return sum;
 };
 
 // Do not edit below this line
--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isFourHundred = year % 400 === 0;
+if (isDivisibleByFour && (!isCentury || isFourHundred)){
+    return true
+}
+else {
+    return false
+}
 };
 
 // Do not edit below this line
